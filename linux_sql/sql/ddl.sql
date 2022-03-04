1ae5449c0975ea7605f4d50888b50161a94e5a38
Jake Vachon <vachon.programmer@gmail.com>
2022-03-04 21:36:06 +0000

    DDL to create tables

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,32 @@
+
+
+CREATE TABLE PUBLIC.host_info
+  (
+     id               SERIAL NOT NULL,
+     hostname         VARCHAR NOT NULL,
+     cpu_number       INT NOT NULL,
+     cpu_architecture VARCHAR NOT NULL,
+     cpu_model        VARCHAR NOT NULL,
+     cpu_mhz          FLOAT NOT NULL,
+     L2_cache         INT NOT NULL,
+     total_mem        INT NOT NULL,
+     time_stamp       TIMESTAMP NOT NULL,
+
+     PRIMARY KEY (id),
+     UNIQUE (hostname)
+  );
+
+CREATE TABLE PUBLIC.host_usage
+ (
+     "timestamp"    TIMESTAMP NOT NULL,
+     host_id        SERIAL NOT NULL,
+     memory_free    INT NOT NULL,
+     cpu_idle       INT NOT NULL,
+     cpu_kernal     INT NOT NULL,
+     disk_io        INT NOT NULL,
+     disk_available INT NOT NULL,
+
+     FOREIGN KEY (host_id)
+        REFERENCES host_info(id)
+  );
+
